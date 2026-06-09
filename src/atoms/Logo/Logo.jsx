f6137ebb6225a4f81d330fbe59c9221ef403a168
lele-maxwell <leleivanlele22@gmail.com>
2026-06-09 15:05:49 +0100

    feat: update logo assets and apply gradient styling to brand name

--- src/atoms/Logo/Logo.jsx
+++ src/atoms/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.css'
 export default function Logo() {
   return (
     <a href="/" className={styles.logo} aria-label="Openflows home">
-      <span className={styles.wave} aria-hidden="true">🌊</span>
+      <img src="/openlogo.svg" alt="Openflows Logo" className={styles.icon} />
       <span className={styles.name}>Openflows</span>
     </a>
   )
