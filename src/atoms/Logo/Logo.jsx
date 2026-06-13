f09e4fbf9361d63ad1a2b54ebe64122ad26d5441
Christian Yemele <yemelechristian2@gmail.com>
2026-06-13 22:57:29 +0100

    feat(site): increase logo size to 64px

--- src/atoms/Logo/Logo.jsx
+++ src/atoms/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.css'
 export default function Logo() {
   return (
     <a href="/" className={styles.logo} aria-label="Openflows home">
-      <img src="/openlogo-transparent.png" alt="Openflows Logo" className={styles.icon} style={{ width: 48, height: 48 }} />
+      <img src="/openlogo-transparent.png" alt="Openflows Logo" className={styles.icon} />
       <span className={styles.name}>Openflows</span>
     </a>
   )
