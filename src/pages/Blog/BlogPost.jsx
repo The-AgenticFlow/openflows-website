270262ab5cda54569b63a6fed5c2d8e3df3047fa
lele-maxwell <leleivanlele22@gmail.com>
2026-06-20 18:37:31 +0100

    refactor: rename shareBtn to shareLinkBtn and clean up CSS override styles

--- src/pages/Blog/BlogPost.jsx
+++ src/pages/Blog/BlogPost.jsx
@@ -193,7 +193,7 @@ export default function BlogPost() {
 
                 {/* Share Button */}
                 <div className={styles.shareBar}>
-                    <button onClick={handleShare} className={styles.shareBtn}>
+                    <button onClick={handleShare} className={styles.shareLinkBtn}>
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                             <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
                             <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
