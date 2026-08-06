39242234cd4d85f04766df58e787072fa392f84e
Shadow <shadow@openflows.dev>
2026-08-06 13:20:59 +0100

    fix: use master branch for Tina Cloud

--- tina/config.ts
+++ tina/config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
+  process.env.GITHUB_REF_NAME ||
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  "main";
+  "master";
 
 export default defineConfig({
   branch,
